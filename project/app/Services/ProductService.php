8721da4c4e1754289115542d7f225cdcb265be8c
Christian Tarifeno <christiantarifer@gmail.com>
2025-03-12 19:56:22 -0500

    Added logger to ProductService and better error handling procedures.

--- project/app/Services/ProductService.php
+++ project/app/Services/ProductService.php
@@ -8,70 +8,115 @@ use App\Validations\ProductValidator;
 class ProductService{
 
     protected $productModel;
+    protected $logger;
 
     public function __construct()
     {
         $this->productModel= new ProductModel();
+        $this->logger = service('logger');
     }
 
     public function getAll(){
-        return $this->productModel->findAll();
+
+        try {
+            return $this->productModel->findAll();
+        } catch (\Throwable $th) {
+           $this->logger->error('Error whil fetching the product:', $th->getMessage());
+           return ['errors' => 'No se pudieron obtener los productos.'];
+        }
+        
     }
 
     public function getById($id){
-        return $this->productModel->find($id);
+
+        try {
+            return $this->productModel->find($id);
+        } catch (\Throwable $th) {
+            $this->logger->error('Error while fetching the product ID {$id}: ' .$th->getMessage());
+            return ['errors' => 'No se pudo encontrar el producto.'];
+        }
+        
     }
 
     public function create(array $data){
 
+        try {
+            $validation = ProductValidator::validate($data);
 
-        $validation = ProductValidator::validate($data);
+            if(!empty($validation['errors'])){
+                return [ 'errors' => $validation['errors'] ];
+            }
 
-        if(!empty($validation['errors'])){
-            return [ 'errors' => $validation['errors'] ];
+            if (!$this->productModel->insert($validation['data'])) {
+            // if (!$this->productModel->insert($data)) {
+                return ['errors' => $this->productModel->errors()];
+            }
+
+            $this->logger->info('Product created'. json_encode($validation['data']));
+            return [
+                'success' => true,
+                'message' => 'Producto creado correctamente.'
+            ];
+        } catch (\Throwable $th) {
+            $this->logger->error('Error while creating product: ' .$th->getMessage());
+            return ['errors' => 'Ocurrió un error al crear el producto.'];
         }
-
-        if (!$this->productModel->insert($data)) {
-            return ['errors' => $this->productModel->errors()];
-        }
-
-        return [
-            'success' => true,
-            'message' => 'Producto creado correctamente.'
-        ];
 
     }
 
     public function update($id, array $data){
 
-        $validation = ProductValidator::validate($data);
+        try {
+            $validation = ProductValidator::validate($data);
 
-        if(!empty($validation['errors'])){
-            return [ 'errors' => $validation['errors'] ];
+            if(!empty($validation['errors'])){
+                return [ 'errors' => $validation['errors'] ];
+            }
+
+            if (!$this->productModel->update($id, $validation['data'])){
+                return [ 'errors' => $this->productModel->errors()];
+            }
+
+            $this->logger->info('Product ID {$id}');
+            return [ 
+                'success' => true,
+                'message' => 'Producto actualizado correctamente.'
+            ];
+        } catch (\Throwable $th) {
+            $this->logger->error('Error updating the product with ID {$id}' .$th->getMessage());
+            return ['errors' => 'Ocurrió un error al actualizar el producto.'];
         }
-
-        if (!$this->productModel->update($id, $data)){
-            return [ 'errors' => $this->productModel->errors()];
-        }
-
-        return [ 
-            'success' => true,
-            'message' => 'Producto actualizado correctamente.'
-        ];
+        
     }
 
     public function delete($id){
-        if (!$this->productModel->delete($id)){
-            return ['error' => 'No se pudo eliminar el producto.'];
+        try {
+            if (!$this->productModel->delete($id)){
+                return ['error' => 'No se pudo eliminar el producto.'];
+            }
+
+            $this->logger->info('Product ID {$id} deleted');
+            return ['success' => 'Producto eliminado correctamente.'];
+        } catch (\Throwable $th) {
+            $this->logger->error('Error while deleting the product: ' .$th->getMessage());
+            return ['errors' => 'Ocurrió un error al eliminar el producto.'];
         }
-        return ['success' => 'Producto eliminado correctamente.'];
+        
     }
 
     public function search($query){
-        return $this->productModel
+        try {
+
+            $this->logger->info('Product: searching for... {$query}');
+            return $this->productModel
                     ->like('name', $query)
                     ->orLike('brand', $query)
                     ->findAll();
+        } catch (\Throwable $th) {
+            $this->logger->error('Error while searching using the parameters: ' .$th->getMessage());
+            return ['errors'=> 'Ocurrió un error al buscar por los parámetros.'];
+        }
+        
     }
 
 }
